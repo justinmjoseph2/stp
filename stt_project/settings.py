2dbefded104f637891e18ebe3cd6f710fcd4cec9
Justin M Joseph <52587929+justinmjoseph2@users.noreply.github.com>
2024-09-09 11:43:43 +0530

    Update settings.py

--- stt_project/settings.py
+++ stt_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-d02*tqe0-y9bxu6m8)%zbp@1p%1k%=5!_5dlzj(wtsb*e)li@5
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
